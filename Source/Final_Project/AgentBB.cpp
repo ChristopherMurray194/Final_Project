3cf1081f8f871ecb736221d6864ffb029fcb0f09
Christopher Murray <chris.murray194@hotmail.co.uk>
2016-04-05 17:47:14 +0100

    Unecessary files finally deleted.

--- Source/Final_Project/AgentBB.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Final_Project.h"
-#include "AgentBB.h"
-
-UAgentBB::UAgentBB()
-{
-
-}
--- Source/Final_Project/AgentBT.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Final_Project.h"
-#include "AgentBT.h"
-
-
-
-
